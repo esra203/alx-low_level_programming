cf6e79c736c0790ac0abda856b1b94930f49dc27
esra203 <esraelshik203@gmail>
2023-08-10 16:50:32 -0700

    good

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stido.h>
+
+/**
+ * main -Entry point 
+ *
+ * Description: A C program that prints  with printf funtion
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n"):
+	retrun (0);
+}	
