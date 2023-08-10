980836283be64e31eea470322188dd5b80564d83
esra203 <esraelshik203@gmail>
2023-08-10 16:17:11 -0700

    good

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/**
+/*
  * main - Entry point 
  *
  * Description: A C program that prints  function
@@ -9,5 +9,5 @@
 int main(void)
 {
 	puts("\" Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }
