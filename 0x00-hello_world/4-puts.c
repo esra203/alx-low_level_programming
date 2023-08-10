c36b36453a435f5119866504a3ac5131265d0816
esra203 <esraelshik203@gmail>
2023-08-10 16:24:27 -0700

    good

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/*
- * main - Entry point 
+/**
+ * main: Entry point
  *
  * Description: A C program that prints  function
  *
@@ -8,6 +8,6 @@
 */
 int main(void)
 {
-	puts("\" Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
