fbe3f1aa580086d35dc5135b3aa3ef5f56d77adf
esra203 <esraelshik203@gmail>
2023-08-11 07:41:25 -0700

    good

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabest
+ *
+ * Description: print the value alphabest
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char apl[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$";
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
