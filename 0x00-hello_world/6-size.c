737ecdbd6f0746a8643502a42c4cabdcd8470e99
esra203 <esraelshik203@gmail>
2023-08-10 18:24:21 -0700

    good

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,12 +3,13 @@
 /**
  * main -Entry point
  *
- * Description: using sizeof to print size
+ * Description: using sizeof to print size of various
  *
  * Return: Always 0 (Success)
 */
+
 int main(void)
-{	
+{
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
