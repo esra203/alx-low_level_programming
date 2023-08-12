7c788ea8cff7dfd145c4b95db6dfdb6194fc9323
esra203 <esraelshik203@gmail>
2023-08-11 17:05:15 -0700

    good

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,22 +7,21 @@
  *
  * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-    char lowercase = 'a';
-    char uppercase = 'A';
+	char lowercase = 'a';
+	char uppercase = 'A';
 
-    while (lowercase <= 'z')
-    {
-	putchar(lowercase);
-	lowercase++;
-    }
-    while (uppercase <= 'Z')
-    {
-	putchar(uppercase);
-	uppercase++;
-    }
-    putchar ('\n');
-    return (0);
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+	while (uppercase <= 'Z')
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
+	putchar ('\n');
+	return (0);
 }
