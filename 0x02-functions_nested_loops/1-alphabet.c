63f54e2dfc36453db698e6ae8f965ed295048fb8
esra203 <esraelshik203@gmail>
2023-08-15 14:51:08 -0700

    good

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,7 @@
 #include "main.h"
 /**
- * main -print alphabet
+ * print_alphabet - print the alphabet a-z
  *
- * Description: A C program that prints  function
- *
- * Return: Always 0 (Success)
 */
 void print_alphabet(void)
 {
