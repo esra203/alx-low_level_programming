5c87c0852cfa13f4eb2ba35bdccc155d81d43b1f
esra203 <esraelshik203@gmail>
2023-08-17 13:36:31 -0700

    good

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,17 +9,19 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (j = 0; j <= 10; j++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if (i >= 10)
-				_putchar ('1');
-			_putchar (i % 10 + '0');
-
+			i = k
+			if (i > 9)
+			{
+				_putchar (1 + 48);
+				k = i % 10;
+			}
+		_putchar (k + 48);
 		}
 		_putchar ('\n');
-	}
 }
