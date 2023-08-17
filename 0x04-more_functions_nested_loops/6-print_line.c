e4bca570486a66b725920eba4f1dd53b3bd95066
esra203 <esraelshik203@gmail>
2023-08-17 15:33:19 -0700

    good

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,32 +1,22 @@
 #include "main.h"
 
 /**
- * print_line - print a straight line
- *
+ * print_line - prints a straight line
  * @n: is the number of the time _character
  * should be printed
+ * Return: void
 */
 
 int print_line(int n)
 {
 	if (n <= 0)
-	{
 		_putchar('\n');
-		return;
-	}
-
-	int lnchr;
-	for (lnchr = 0; lnchr < n; lnchr++)
+	else
 	{
+		int lnchr;
+
+		for (lnchr = 0; lnchr < n; lnchr++)
 			_putchar('_');
-	}
 		_putchar('\n');
-}
-
-int main(void)
-{
-print_line(5);
-print_line(0);
-print_line(3);
-return 0;
+	}
 }
