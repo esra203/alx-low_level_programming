7d6eda8f0ac8438aa2c2de5f4e22b47668089cb5
esra203 <esraelshik203@gmail>
2023-08-17 13:08:33 -0700

    good

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar ('i');
+		}
+		_putchar ('\n');
+	}
+}
