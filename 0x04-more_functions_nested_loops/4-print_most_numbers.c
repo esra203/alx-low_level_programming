9937ef947ed5e4e23cddfc378ea93f6a68fabc91
esra203 <esraelshik203@gmail>
2023-08-17 12:14:05 -0700

    good

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,9 +12,9 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(c == '2' || c == '4'))
+		if (c == '2' || c == '4')
 			continue;
-			_putchar (c);
+			_putchar (c + 48);
 	}
 	_putchar ('\n');
 }
