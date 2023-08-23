318c1b6f8138fccdd8ade6f293d5fbc554000bfb
esra203 <esraelshik203@gmail>
2023-08-23 09:19:50 -0700

    good

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,15 +14,17 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
+	/*find the size of dest array*/
 	while (dest[i])
 	{
 		i++;
 	}
+	/*iterate through each src array value without the null byte*/
 	j = 0;
 	while (src[j])
 	{
+		/*append src[j] to dest[i] while overwrittingthe null byte in dest*/
 		dest[i++] = src[j];
-		i++;
 		j++;
 	}
 	return (dest);
