846a752c0ec3e6b74ba36155052c480b09bfe503
esra203 <esraelshik203@gmail>
2023-08-23 09:07:21 -0700

    good

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,18 +14,16 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
-	while (dest[i] != '\0')
+	while (dest[i])
 	{
 		i++;
 	}
 	j = 0;
-	while (src[j] != '\0')
+	while (src[j])
 	{
-		dest[i] = src[j];
+		dest[i++] = src[j];
 		i++;
 		j++;
 	}
-
-	dest[i] = '\0';
 	return (dest);
 }
