77a366b103eb997e5020a64f05676a0c09defa77
esra203 <esraelshik203@gmail>
2023-09-01 15:40:51 -0700

    good

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,9 +1,6 @@
 #include "main.h"
+#include <stdio.h>
 
-/**
- * main - print the words
- * Return: 0
-*/
 int main(void)
 {
 _puts("\"At the end of the day,my goal was to be the best hacker\"\n\t- Kevin Mitnick");
