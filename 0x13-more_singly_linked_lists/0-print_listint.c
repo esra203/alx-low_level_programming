5ce5b164467b98d278673d743f53fa5aa618fe66
esra203 <esraelshik203@gmail>
2023-09-25 15:16:01 -0700

    good

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t count;
 
-	while (h)
+	if (h == NULL)
+		return (0);
+	for (count = 0; h != NULL; count++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		num++;
 	}
-	return (num);
+	return (count);
 }
 
